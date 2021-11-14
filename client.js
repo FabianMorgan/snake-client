5f9bef88667d41b2117f1ba46cb45b42f1bc2b59
Fabian Morgan <fabagan@yahoo.com>
2021-11-13 20:54:54 -0500

    created constants.js

--- client.js
+++ client.js
@@ -1,8 +1,10 @@
+const {IP, PORT} = require('./constants');
+
+
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: '165.227.47.243', // IP address here,
-    port: 50541 // PORT number here,
+    
   });
 };
 
